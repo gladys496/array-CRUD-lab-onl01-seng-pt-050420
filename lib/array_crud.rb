c33dfc33556f2936d2fed45267f611eeef40596d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-05-25 15:54:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,15 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  array = ["sun", "apple", "today", "book"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+
   
 end
 
